00425440e9e6cddb736e83d2bc609f634d7a76dd
Agustin Moya <agustinmarcelomoya@gmail.com>
2023-09-19 23:31:47 +0100

    Support dark and light themes. Add a mode toggle on the page to easily change themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/theme-toggle"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <ModeToggle />
         <Button variant="outline">Button</Button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
